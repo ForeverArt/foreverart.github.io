8f744dfd8b4d86f25952993be3800b8c6d43a215
ForeverArt <luxiuming@luxiumingdeMacBook-Pro.local>
2017-08-26 22:33:10 +0800

    100💗

---
+++ js/100.js
@@ -0,0 +1,16 @@
+console.group('Hey~')
+console.log('有木有被这个界面丑哭，哈哈哈哈哈 =。=');
+console.log('这两天想着见你的时候给你带什么, 想的头都大了, 最后还是决定来一波丑炫丑炫的！')
+console.log('emmmmmmmmm');
+console.groupEnd()
+console.group('心里话环节')
+console.log('不知不觉已经过去100天啦，说起来挺忐忑，一百天后的你对我是更喜欢了还是多了小抱怨呢QAQ')
+console.log('感觉真是没干啥让小可爱开心的事儿，净添堵了（可不是咋的）');
+console.log('有那么多的时候我不像自己想象的那么有耐心，明明看见了你的许多努力却从不赞扬，当作了理所当然');
+console.log('想一想，你却一直包容着我的坏脾气，从不怪责我，哎呀就觉得你贼温油（其实你没事儿给我一拳也蛮好的，哈哈）');
+console.log('不过傻子也有一颗向上的❤️，我要让自己更傻一点，这样你就能放心欺负我了');
+console.log('你看傻子还能编程编出朵花儿来呢，想一想是不是开心多了！');
+console.groupEnd()
+console.group('(*.*)')
+console.log('明天会更好，我明天会更傻，你明天会更可爱！哈哈哈哈哈！');
+console.log('还请多多指教！mua～');
